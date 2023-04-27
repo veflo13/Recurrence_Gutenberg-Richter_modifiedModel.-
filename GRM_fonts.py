ffc0ed953602b6294ce610bb09f7bd84b92cf42b
Verónica Flores Gutiérrez <113630553+veflo13@users.noreply.github.com>
2023-04-27 20:32:02 +0000

    for to number of fonts done

--- GRM_fonts.py
+++ GRM_fonts.py
@@ -7,10 +7,13 @@
 import numpy as np
 minf = 4.2
 maxf = 8.3
+nfonts = 5
+F = []
 F1 = np.arange(minf, maxf, 0.1)
 print (F1)
 
-nfonts = 5
-for i in range(nfonts+1):
-     F='F',i
-     print(F)
+
+for i  in range(nfonts+1):
+     F= F + [i]
+
+print(F)
